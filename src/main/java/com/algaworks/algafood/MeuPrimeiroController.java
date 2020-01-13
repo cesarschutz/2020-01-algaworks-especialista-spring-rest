98676a21f05a78369d02a1b50321cd9c3e83ac9f
Cesar Schutz <cesarschutz340@gmail.com>
2020-01-12 21:01:24 -0300

    Aula 2.11

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -1,14 +1,16 @@
-package com.algaworks.algafood.controllers;
+package com.algaworks.algafood;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller()
+@Controller
 public class MeuPrimeiroController {
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello(){
-        return "eeeeidiii2";
-    }
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "Hello!";
+	}
+	
 }
