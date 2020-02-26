183428023937a7497bc94eab53ba17a70834cf5e
Cesar Schutz <cesarschutz340@gmail.com>
2020-02-26 19:31:18 -0300

    3.18. A anotação @JoinColumn

--- Capitulo 03/algafood-api/src/main/resources/import.sql
+++ Capitulo 03/algafood-api/src/main/resources/import.sql
@@ -1,7 +1,7 @@
 insert into cozinha (id, nome) values (1, 'Tailandesa');
 insert into cozinha (id, nome) values (2, 'Indiana');
 insert into cozinha (id, nome) values (3, 'Peruana'); /* Não utilizar essa cozinha em relacionamento pois ela é utilizada no teste de remover() */
-insert into restaurante (nome, taxa_frete, cozinha_codigo) values ('Thai Gourmet', 10, 1);
-insert into restaurante (nome, taxa_frete, cozinha_codigo) values ('Thai Delivery', 9.50, 2);
-insert into restaurante (nome, taxa_frete, cozinha_codigo) values ('Tuk Tuk Comida Indiana', 15, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50, 2);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 1);
 
